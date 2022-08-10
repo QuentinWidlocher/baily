8c067ecee89bc896ec179819b5bb049c4c4aee30
QuentinWidlocher <qwidlocher@gmail.com>
2022-08-10 18:29:20 +0200

    🔧  Configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx}"],
+  content: ["./app/**/*.{ts,tsx,js,jsx,html}"],
   theme: {
     extend: {},
   },
