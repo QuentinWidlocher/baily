54a2856c42532dddae74082b9a9ad37720d05133
Quentin WIDLOCHER <quentin.widlocher@erudo.fr>
2022-08-10 17:01:59 +0200

    🐛 Fix tailwind

--- app/routes/tailwindcss.tsx
+++ app/routes/tailwindcss.tsx
@@ -1,4 +1,4 @@
 import { LoaderFunction } from "@remix-run/node";
 import { serveTailwindCss } from "remix-tailwind";
 
-export const loader: LoaderFunction = () => serveTailwindCss("app/style.css");
+export const loader: LoaderFunction = () => serveTailwindCss();
