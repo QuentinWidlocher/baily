fc18a68f275a7b23bb95ebf7bbffeec7d0ffd2bf
Quentin WIDLOCHER <quentin.widlocher@erudo.fr>
2022-08-11 12:19:01 +0200

    🚑 Add logs

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,10 @@
-import { redirect } from "~/services/superjson";
+import { redirect } from '~/services/superjson'
 
 export async function loader() {
+  console.log(process.env)
   const babyId =
-    process.env.NODE_ENV == "production" && process.env.CONTEXT == "production"
+    process.env.NODE_ENV == 'production' && process.env.CONTEXT == 'production'
       ? process.env.DEFAULT_BABY_ID
-      : process.env.DEPLOY_PREVIEW_DEFAULT_BABY_ID;
-  return redirect(`/baby/${babyId}`);
+      : process.env.DEPLOY_PREVIEW_DEFAULT_BABY_ID
+  return redirect(`/baby/${babyId}`)
 }
