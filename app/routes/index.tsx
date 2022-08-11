5c1a2b88804e20894026d8b3059195e6c221761e
Quentin WIDLOCHER <quentin.widlocher@erudo.fr>
2022-08-11 09:36:31 +0200

    🐛 Fix env var

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,9 @@
-import { redirect } from '~/services/superjson'
+import { redirect } from "~/services/superjson";
 
 export async function loader() {
   const babyId =
-    process.env.NODE_ENV == 'production'
+    process.env.NODE_ENV == "production" && process.env.CONTEXT == "production"
       ? process.env.DEFAULT_BABY_ID
-      : process.env.DEPLOY_PREVIEW_DEFAULT_BABY_ID
-  return redirect(`/baby/${babyId}`)
+      : process.env.DEPLOY_PREVIEW_DEFAULT_BABY_ID;
+  return redirect(`/baby/${babyId}`);
 }
