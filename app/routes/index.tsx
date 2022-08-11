2d47429b9e16aba7b2572686c4006ed6de065800
Quentin WIDLOCHER <quentin.widlocher@erudo.fr>
2022-08-11 12:23:03 +0200

    🚑 Hotfix

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,8 @@ import { redirect } from '~/services/superjson'
 export async function loader() {
   console.log(process.env)
   const babyId =
-    process.env.NODE_ENV == 'production' && process.env.CONTEXT == 'production'
+    process.env.NODE_ENV == 'production' &&
+    process.env.URL == 'https://b-plus-plus.netlify.app'
       ? process.env.DEFAULT_BABY_ID
       : process.env.DEPLOY_PREVIEW_DEFAULT_BABY_ID
   return redirect(`/baby/${babyId}`)
