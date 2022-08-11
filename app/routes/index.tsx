2c8ea24755238659150b492db4a5d341d29850c8
Quentin WIDLOCHER <quentin.widlocher@erudo.fr>
2022-08-11 13:02:56 +0200

    🔧 Configure netlify env var

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,10 +2,5 @@ import { redirect } from '~/services/superjson'
 
 export async function loader() {
   console.log(process.env)
-  const babyId =
-    process.env.NODE_ENV == 'production' &&
-    process.env.URL == 'https://b-plus-plus.netlify.app'
-      ? process.env.DEFAULT_BABY_ID
-      : process.env.DEPLOY_PREVIEW_DEFAULT_BABY_ID
-  return redirect(`/baby/${babyId}`)
+  return redirect(`/baby/${process.env.DEFAULT_BABY_ID}`)
 }
