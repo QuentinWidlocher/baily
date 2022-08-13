a9f91930c2decdd02909364b0265cefd0670152c
QuentinWidlocher <qwidlocher@gmail.com>
2022-08-13 16:39:34 +0200

    🔧  Configure Netlify

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,5 +2,5 @@ import { redirect } from '~/services/superjson'
 
 export async function loader() {
   console.log(process.env)
-  return redirect(`/baby/${process.env.DEFAULT_BABY_ID}`)
+  return redirect(`/baby/${process.env.DEFAULT_BABY}`)
 }
