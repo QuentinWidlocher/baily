adf0a109c7845375327348fce325a43ad310ef9d
QuentinWidlocher <qwidlocher@gmail.com>
2022-08-14 18:34:37 +0200

    ✨ Add quick bottle

---
+++ app/routes/index.ts
@@ -0,0 +1,5 @@
+import { redirect } from '~/services/superjson'
+
+export async function loader() {
+  return redirect(`/baby/${process.env.DEFAULT_BABY}`)
+}
--- app/routes/quick-add-bottle.ts
+++ app/routes/quick-add-bottle.ts
@@ -1,6 +1,5 @@
 import { redirect } from '~/services/superjson'
 
 export async function loader() {
-  console.log(process.env)
-  return redirect(`/baby/${process.env.DEFAULT_BABY}`)
+  return redirect(`/baby/${process.env.DEFAULT_BABY}/bottle/new`)
 }
